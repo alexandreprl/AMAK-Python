1cb863852399b1857995a44735e84ed2c641d145
Alexandre <alexandre.perles@gmail.com>
2024-11-06 16:20:25 +0100

    Fix module

---
+++ amak/__init__.py
@@ -0,0 +1,1 @@
+from amak.amak import MAS, Agent, Scheduler
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='amak',
     packages=find_packages(include=['amak']),
-    version='0.1.1',
+    version='0.1.2',
     description='Simplified AMAK for Python',
     author='Alexandre Perles',
 )
