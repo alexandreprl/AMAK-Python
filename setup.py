cc2555cf4bf5de49e8276e7f85af630f56b7bb51
Alexandre <alexandre.perles@gmail.com>
2024-12-13 11:19:10 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='amak',
     packages=find_packages(include=['amak']),
-    version='0.1.2',
+    version='0.2.0',
     description='Simplified AMAK for Python',
     author='Alexandre Perles',
 )
