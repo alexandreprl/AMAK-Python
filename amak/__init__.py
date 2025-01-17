f8ca4f039469379612512a16633f47aea7553c13
Alexandre <alexandre.perles@gmail.com>
2025-01-17 16:49:45 +0100

    Render agent and environment entities

--- amak/__init__.py
+++ amak/__init__.py
@@ -1,3 +1,3 @@
 from amak.amak import MAS, Agent, Scheduler
-from amak.amakpygame import AMAKPygame
+from amak.amakpygame import AMAKPygame, AgentEntity, EnvironmentEntity
 from amak.avt import AVT
--- amak/amakpygame.py
+++ amak/amakpygame.py
@@ -1,16 +1,20 @@
 import pygame
 
+from amak import amak
+
 
 class AMAKPygame:
-    def __init__(self, amas, environment):
+    def __init__(self, amas, environment, width=640, height=480, fps=60):
         self._running = False
         self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+        self.fps = fps
+        self.size = width, height
         self.amas = amas
         self.environment = environment
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         pygame.display.set_caption("AMAK - Python")
+        self.clock = pygame.time.Clock()
         self.start()
 
     def on_event(self, event):
@@ -29,4 +33,37 @@ class AMAKPygame:
             self.amas.cycle()
             self.environment.cycle()
 
+            self._display_surf.fill((0, 0, 0))
+            self.environment.render(self._display_surf)
+
+            for entity in self.amas.agents:
+                if entity.surface and entity.rect:
+                    self._display_surf.blit(entity.surface, entity.rect)
+
+            pygame.display.update()
+            self.clock.tick(self.fps)
         pygame.quit()
+
+
+class AgentEntity(amak.Agent):
+    def __init__(self, mas, initial_position, color=(255, 255, 255)):
+        super().__init__(mas)
+        self.surface = pygame.Surface((10, 10))
+        self.surface.fill(color)
+        self.rect = self.surface.get_rect(center=initial_position)
+
+    def set_position(self, position):
+        self.rect.center = position
+
+
+class EnvironmentEntity:
+    def __init__(self, initial_position, color=(255, 255, 255)):
+        self.surface = pygame.Surface((10, 10))
+        self.surface.fill(color)
+        self.rect = self.surface.get_rect(center=initial_position)
+
+    def set_position(self, position):
+        self.rect.center = position
+
+    def set_color(self, color):
+        self.surface.fill(color)
